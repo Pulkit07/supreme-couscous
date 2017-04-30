60c4e20a5886562a5dc731d04a1d40c7d8c22bcd
Pulkit07 <7895pulkit@gmail.com>
2017-05-01 02:15:17 +0530

    update the definition of SignUpForm class
    First name and last name are required, can't be marked as optional. Removed some
    unwanted help text and added a new optional text box of bio.

--- rocket/forms.py
+++ rocket/forms.py
@@ -3,13 +3,15 @@ from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    first_name=forms.CharField(max_length=40,required=False,help_text='optional')
-    last_name=forms.CharField(max_length=40,required=False,help_text='optional')
-    email=forms.EmailField(max_length=250,help_text='please inform a valid email address')
+    first_name = forms.CharField(max_length = 40)
+    last_name = forms.CharField(max_length = 40)
+    email = forms.EmailField(max_length = 250)
+    bio = forms.CharField(max_length = 500, required = False)
 
     class Meta:
-        model=User
+        model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
 
 class EditProfileForm(UserChangeForm):
 
