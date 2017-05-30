b737ca6bbdaa61294f89119237cca130635d44c3
Pulkit Goyal <7895pulkit@gmail.com>
2017-05-30 18:38:31 +0530

    errors: fixing some errors which were left

--- rocket/urls.py
+++ rocket/urls.py
@@ -15,7 +15,7 @@ from .views import (
 )
 
 urlpatterns = [
-    url(r'^login/$', loginv.as_view(), name='login'),
+    url(r'^login/$', login.as_view(), name='login'),
     url(r'^signup/$', signup.as_view(), name='signup'),
     url(r'^logout/$', logout.as_view(), name='logout'),
     url(r'^profile/$', Profile.as_view(), name='profile'),
@@ -23,7 +23,7 @@ urlpatterns = [
         name='profile_picture_upload'),
     url(r'^edit_profile/$', edit_profile, name='edit_profile'),
     url(r'^change_password/$', change_password, name='change_password'),
-    url(r'^$', home),
+    url(r'^$', home.as_view()),
     url(r'^grievances/$', grievances.as_view(), name="grievances"),
     url(r'^activate/(?P<rhash>[0-9A-Za-z]+)/?$',
         activateuser.as_view(), name='activation_link'),
